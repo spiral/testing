142a4b11704b9bc501891e43044cfd06ac729deb
Pavel Buchnev <butschster@gmail.com>
2022-02-03 01:33:58 +0300

    Initial commit

---
+++ tests/src/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace VendorName\Skeleton\Tests;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+    use MockeryPHPUnitIntegration;
+}
