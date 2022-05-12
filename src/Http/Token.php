e77f12c812e601e5054b9a371112b207d1e916b9
butschster <butschster@gmail.com>
2022-05-12 15:05:26 +0300

    Fixes problem with using fake actor

---
+++ src/Http/Token.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Testing\Http;
+
+use Spiral\Auth\TokenInterface;
+
+class Token implements TokenInterface
+{
+    /** @var string */
+    private $id;
+
+    /** @var \DateTimeInterface|null */
+    private $expiresAt;
+
+    /** @var array */
+    private $payload;
+
+    public function __construct(string $id, array $payload, \DateTimeInterface $expiresAt = null)
+    {
+        $this->id = $id;
+        $this->expiresAt = $expiresAt;
+        $this->payload = $payload;
+    }
+
+    public function getID(): string
+    {
+        return $this->id;
+    }
+
+    public function getExpiresAt(): ?\DateTimeInterface
+    {
+        return $this->expiresAt;
+    }
+
+    public function getPayload(): array
+    {
+        return $this->payload;
+    }
+}
