1cd83fe14a3e4de3f5d9253f60a3a21c95f4a95e
butschster <butschster@gmail.com>
2022-05-26 12:06:00 +0400

    Adds update config method

--- src/Traits/InteractsWithConfig.php
+++ src/Traits/InteractsWithConfig.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Spiral\Testing\Traits;
 
 use Spiral\Config\ConfiguratorInterface;
+use Spiral\Config\Patch\Set;
 use Spiral\Core\ConfigsInterface;
 
 trait InteractsWithConfig
@@ -12,19 +13,34 @@ trait InteractsWithConfig
     public function assertConfigMatches(string $name, array $data): void
     {
         $config = $this->getConfig($name);
+
         $this->assertSame($data, $config);
     }
 
     public function getConfig(string $config): array
     {
-        return $this->getContainer()->get(ConfigsInterface::class)->getConfig($config);
+        return $this->getConfigs()->getConfig($config);
+    }
+
+    public function getConfigurator(): ConfiguratorInterface
+    {
+        return $this->getContainer()->get(ConfiguratorInterface::class);
+    }
+
+    public function getConfigs(): ConfigsInterface
+    {
+        return $this->getContainer()->get(ConfigsInterface::class);
     }
 
     public function setConfig(string $config, array $data): void
     {
-        $this->getContainer()->get(ConfiguratorInterface::class)->setDefaults(
-            $config,
-            $data
-        );
+        $this->getConfigurator()->setDefaults($config, $data);
+    }
+
+    public function updateConfig(string $key, mixed $data): void
+    {
+        [$config, $key] = explode('.', $key, 2);
+
+        $this->getConfigs()->modify($config, new Set($key, $data));
     }
 }
